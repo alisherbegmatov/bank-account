dc7ced98682f8daac814ebbaa1497693907c28c5
alisherbegmatov <60242355+alisherbegmatov@users.noreply.github.com>
2020-11-07 18:40:36 -0800

    Defined 3 different bank account examples
    Defined 3 different bank account examples

--- BankAccount.py
+++ BankAccount.py
@@ -43,3 +43,19 @@ class BankAccount:
             Routing No.: {self.routing_number}
             Balance: {self.balance}
         """)
+
+joi_account = BankAccount("Joi Anderson", 123456789, 987654321, 1234)
+joi_account.get_balance()
+joi_account.add_interest()
+joi_account.print_receipt()
+
+alisher_account = BankAccount("Alisher Begmatov", 123456789, 987654321, 1234)
+alisher_account.withdraw(777)
+alisher_account.deposit(21000)
+alisher_account.print_receipt()
+
+user_account = BankAccount("User Account", 123456789, 987654321, 1234)
+user_account.get_balance()
+user_account.add_interest()
+user_account.deposit(10000)
+user_account.print_receipt()
